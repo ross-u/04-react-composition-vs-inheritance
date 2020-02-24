e37121543fb27e479f2429b690a5a4ea3b06634d
ross-u <cirkovic.uros.srb@gmail.com>
2020-02-24 16:25:56 +0100

    Update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import React, { Component } from "react";
 import "./App.css";
 
+import FancyBorder from "./FancyBorder";
+
 // React class Component
 class App extends Component {
   render() {
-    return <div className="App"></div>;
+    return (
+      <div className="App">
+        <FancyBorder title="Fancy Border Example" bottom={<h3>HELLO</h3>}>
+          <h1>Welcome</h1>
+          <p>Thank you for visiting our spacecraft!</p>
+        </FancyBorder>
+      </div>
+    );
   }
 }
 
--- src/FancyBorder.js
+++ src/FancyBorder.js
@@ -1,1 +1,16 @@
+import React from "react";
+import "./FancyBorder.css";
 
+function FancyBorder(props) {
+  return (
+    <div className="fancy">
+      <h3> {props.title} </h3>
+      {props.children}
+      {props.bottom}
+    </div>
+  );
+}
+
+export default FancyBorder;
+
+// rfce + Tab
